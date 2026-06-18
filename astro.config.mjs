154ab75ab93ec38ef2d1fc2e71d6c7007bbdc125
Alexiz248 <alexisfyd@gmail.com>
2026-06-17 19:40:05 -0500

    Change base path from '/SEMS-Landing-Page' to '/Landing-Page'

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 // Si usas un dominio propio o publicas en la raíz, cambia `site` y borra `base`.
 export default defineConfig({
   site: 'https://smart-energy-management-system-sems.github.io',
-  base: '/SEMS-Landing-Page',
+  base: '/Landing-Page',
   vite: {
     plugins: [tailwindcss()],
   },
